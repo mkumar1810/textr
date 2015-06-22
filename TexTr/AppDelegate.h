21b35b3c4e6d32da1df16899038a4d454e294a16
Mohan Kumar <mkumar@skyvo.net>
2015-06-22 15:13:47 +0530

    Initial Commit

---
+++ TexTr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TexTr
+//
+//  Created by Mohan Kumar on 22/06/15.
+//  Copyright (c) 2015 Enter key solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TexTr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TexTr
+//
+//  Created by Mohan Kumar on 22/06/15.
+//  Copyright (c) 2015 Enter key solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
